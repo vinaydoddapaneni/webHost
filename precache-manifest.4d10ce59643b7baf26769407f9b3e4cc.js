7972141e9d1a850a3f44ba0e035200033d484709
vinaydoddapaneni <vinaydoddapaneni@gmail.com>
2019-07-31 11:02:31 +0530

    Updates

---
+++ precache-manifest.4d10ce59643b7baf26769407f9b3e4cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74af1c80ea8117067825ad2ea46936de",
+    "url": "/webHost/index.html"
+  },
+  {
+    "revision": "abe98166b823b7882cd9",
+    "url": "/webHost/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dff8f5e6896d4e47fb65",
+    "url": "/webHost/static/js/2.d16ba94f.chunk.js"
+  },
+  {
+    "revision": "abe98166b823b7882cd9",
+    "url": "/webHost/static/js/main.b8ef6bcb.chunk.js"
+  },
+  {
+    "revision": "9dccec26eaf3d7d96066",
+    "url": "/webHost/static/js/runtime~main.6961fb89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webHost/static/media/logo.5d5d9eef.svg"
+  }
+]);
