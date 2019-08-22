03cf571de8ca475ff372bffd531210d986894454
vinaydoddapaneni <vinaydoddapaneni@gmail.com>
2019-08-22 11:14:38 +0530

    Updates

---
+++ precache-manifest.dcfa652991aaf40843042730620a40aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb917d100cebe84965afd3420e4ad699",
+    "url": "/webHost/index.html"
+  },
+  {
+    "revision": "685ac812ca21f52d8a21",
+    "url": "/webHost/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/webHost/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "685ac812ca21f52d8a21",
+    "url": "/webHost/static/js/main.df695c4b.chunk.js"
+  },
+  {
+    "revision": "9dccec26eaf3d7d96066",
+    "url": "/webHost/static/js/runtime~main.6961fb89.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/webHost/precache-manifest.4cdec107097c6be3168fa8ebb614aa00.js"
+  "/webHost/precache-manifest.dcfa652991aaf40843042730620a40aa.js"
 );
 
 self.addEventListener('message', (event) => {
